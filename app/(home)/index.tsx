53bcb167af33426b6eaea2c68cdc8819e81b2ba3
ywonchae1 <ywonchae62@gmail.com>
2024-09-05 18:58:24 +0900

    fix: google login 403 오류 해결

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Dimensions } from 'react-native';
+import { SafeAreaView, StyleSheet, Dimensions, Platform } from 'react-native';
 import WebView from 'react-native-webview';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 export default function HomeScreen() {
+  const customUserAgent = Platform.OS === 'android'
+  ? 'Chrome/18.0.1025.133 Mobile Safari/535.19'
+  : 'AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75';
   return (
     <>
       <StatusBar style="dark" backgroundColor="#ffffff" />
@@ -13,6 +16,7 @@ export default function HomeScreen() {
         <WebView
           style={styles.webview}
           source={{ uri: 'https://swimie.life' }}
+          userAgent={customUserAgent}
         />
       </SafeAreaView>
     </>
