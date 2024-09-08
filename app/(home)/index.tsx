2a609897786f6f7b772f5bbc95544846ae47495d
Jungjjeong <sky990106@dgu.ac.kr>
2024-09-09 02:04:41 +0900

    style: 웹뷰 스타일 수정

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,16 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Dimensions } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import WebView from 'react-native-webview';
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-
 export default function HomeScreen() {
+  const customUserAgent = 'customUserAgent';
+
   return (
     <SafeAreaView style={styles.container}>
       <WebView
         style={styles.webview}
+        contentInsetAdjustmentBehavior="never"
         source={{ uri: 'https://swimie.life' }}
+        userAgent={customUserAgent}
+        originWhitelist={['*']}
+        scrollEnabled
+        allowsInlineMediaPlayback
+        mediaCapturePermissionGrantType="grantIfSameHostElsePrompt"
       />
     </SafeAreaView>
   );
@@ -19,13 +23,9 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-between',
     backgroundColor: '#ffffff',
   },
   webview: {
     flex: 1,
-    width: windowWidth,
-    height: windowHeight,
   },
 });
